adced7f7f218e9462db7ab56357dc5764a8d3d35
Myryitzy <104706728+Itzelmariana@users.noreply.github.com>
2022-07-16 01:30:12 -0500

    added functions

---
+++ README.md
@@ -0,0 +1,62 @@
+# asdf
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+ asdf
+- asdf
+- asdf
+- asdf
+- asdf
+
+## Installation
+asdf
+
+## Usage
+
+Here we can put detailed stuff.
+
+Including a link to an image [imagetext](imageurl).
+
+That way you have to write less code and can show your
+teacher you used more of the API for inquiererererre.
+
+
+asdf
+
+ ![asdf](asdf)
+
+ adsf
+
+ ![adsf](asdf)
+
+ asdf
+ 
+ ![asdf](asdf)
+
+
+## Credits
+- asdf
+- asdf
+- sooooo much typing
+
+## License
+
+"This application is under the [MIT](https://opensource.org/licenses/MIT) license.
+
+
+## Features
+
+- none
+
+## How to Contribute
+
+- lemmeseeit
+
+## Test
+
+- please
+
+# Questions
+
+If you have any questions you can reach out to me at now
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,18 +1,43 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
+  if (license === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "BSD") {
+    return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+  } else if (license === "EPL") {
+    return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
+  } else if (license === "GNU GPL v3") {
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-
+  if (license === "MIT") {
+    return "https://opensource.org/licenses/MIT";
+  } else if (license === "BSD") {
+    return "https://opensource.org/licenses/BSD-3-Clause";
+  } else if (license === "EPL") {
+    return "https://opensource.org/licenses/EPL-2.0";
+  } else if (license === "GNU GPL v3") {
+    return "https://opensource.org/licenses/gpl-license";
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license == "") {
+    return "";
+  }
+
+  return `"This application is under the [${license}](${renderLicenseLink(
+    license
+  )}) license.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown({
@@ -22,17 +47,27 @@ function generateMarkdown({
   why,
   solve,
   learn,
-  instalation,
+  install,
   titleImg1,
   titleImg2,
   titleImg3,
+  instructions,
   pathImg1,
   pathImg2,
   pathImg3,
-  collaborators
-
+  collaborators,
+  github,
+  sources,
+  license,
+  features,
+  contribute,
+  test,
+  email,
 }) {
   return `# ${title}
+
+${renderLicenseBadge(license)}
+
 ## Description
  ${description}
 - ${motivation}
@@ -40,25 +75,51 @@ function generateMarkdown({
 - ${solve}
 - ${learn}
 
-## Instalation
-${instalation}
+## Installation
+${install}
 
 ## Usage
 
+${instructions}
+
 ${titleImg1}
+
  ![${titleImg1}](${pathImg1})
 
  ${titleImg2}
+
  ![${titleImg2}](${pathImg2})
 
  ${titleImg3}
- ![${titleImg3}](${pathImg3}
+
+ ![${titleImg3}](${pathImg3})
 
 
 ## Credits
+
 ${collaborators}
+${sources}
 
 ## License
+
+${renderLicenseSection(license)}
+
+
+## Features
+
+${features}
+
+## How to Contribute
+
+${contribute}
+
+## Test
+
+${test}
+
+# Questions
+
+If you have any questions you can reach out to me at ${email}
 `;
 }
 
